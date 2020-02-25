f12dda448e1a429c7333139c6c235b2f5cb9fcb0
radville <laura.radville@gmail.com>
2020-02-25 11:40:52 -0500

    don't display 'm^2' if no area is present

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 const Legend = (props) => {
+    function returnArea() {
+        if (props.area) {
+            return props.area + " m^2"
+        }
+    }
+
     return (
         <div id="info" className="esriSimpleSlider">
             <h4>Calculate Area</h4>
             Draw a polygon over the area you would like to measure.
             <br /><br />
-            <span className="label">Area:</span> {props.area} m^2 <span id="area"></span>
+            <span className="label">Area:</span> {returnArea() }<span id="area"></span>
         </div>
     );
 }
